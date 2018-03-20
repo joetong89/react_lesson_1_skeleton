b8c925870d969bd0998b94c2c93a512e585e0144
robmyers82 <robert_myers_@hotmail.com>
2018-03-20 09:52:16 -0300

    Initial Commit

---
+++ public/scripts/app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// action when a form is submitted. In this case,
+// we add the player
+var formDidSubmit = function formDidSubmit(e) {
+    e.preventDefault();
+};
+
+// clear the entire player list
+var clearAllPlayers = function clearAllPlayers() {};
+
+// render the view
+// NOTE: this is a temporary solution! When we work with
+//       React Components we will have a more elegant way
+//       to do this.
+var renderView = function renderView() {};
+
+// call renderView() to initially render the screen.
+renderView();
---
+++ src/app.js
@@ -0,0 +1,22 @@
+// action when a form is submitted. In this case,
+// we add the player
+const formDidSubmit = (e) => {
+    e.preventDefault();
+    
+};
+
+// clear the entire player list
+const clearAllPlayers = () => {
+
+};
+
+// render the view
+// NOTE: this is a temporary solution! When we work with
+//       React Components we will have a more elegant way
+//       to do this.
+const renderView = () => {
+    
+};
+
+// call renderView() to initially render the screen.
+renderView();
